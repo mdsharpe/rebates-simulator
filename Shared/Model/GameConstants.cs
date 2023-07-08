966426016994eb478bcb925a6837129e1e2d2b1f
Mark Williams <mark.williams@enable.com>
2023-07-08 19:24:30 +0100

    b a l a n c e

--- Shared/Model/GameConstants.cs
+++ Shared/Model/GameConstants.cs
@@ -2,9 +2,9 @@
 {
     public static class GameConstants
     {
-        public const decimal ProductManufactureCost = 100;
+        public const decimal ProductManufactureCost = 20;
         public const int TruckCapacity = 10;
-        public const decimal SellPrice = 120;
+        public const decimal SellPrice = 100;
         public const decimal FineAmount = 200;
         public const int Rent = 500;
         public const int RentIntervalSeconds = 15;
