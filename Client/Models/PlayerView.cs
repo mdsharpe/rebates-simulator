fe28e6bef2c0b2b6c50aff6b9a2b90615c4c9827
Matt Sharpe <mds@live.co.uk>
2023-07-08 19:43:57 +0100

    wip

---
+++ Client/Models/PlayerView.cs
@@ -0,0 +1,11 @@
+﻿namespace RebatesSimulator.Client.Models
+{
+    public class PlayerView
+    {
+        public required int Id { get; init; }
+        public required string Name { get; init; }
+        public required decimal Balance { get; init; }
+        public required int Stock { get; init; }
+        public required bool IsCurrentPlayer { get; init; }
+    }
+}
