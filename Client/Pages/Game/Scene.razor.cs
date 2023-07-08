7f16a29d88f488b8f8d996798c7c67825d13ce3e
Mark Williams <mark.williams@enable.com>
2023-07-08 13:04:46 +0100

    Picture in canvas sorta

---
+++ Client/Pages/Game/Scene.razor.cs
@@ -0,0 +1,28 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas;
+using Blazor.Extensions.Canvas.Canvas2D;
+using Microsoft.AspNetCore.Components;
+
+namespace RebatesSimulator.Client.Pages.Game
+{
+    public partial class Scene : ComponentBase
+    {
+        protected BECanvasComponent CanvasComponent = new();
+        private Canvas2DContext? _canvas;
+        protected ElementReference Scenery;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            _canvas = await CanvasComponent.CreateCanvas2DAsync();
+
+            Console.WriteLine("Drawing image");
+            await _canvas.DrawImageAsync(Scenery, 0, 0);
+
+            base.OnAfterRender(firstRender);
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+        }
+    }
+}
